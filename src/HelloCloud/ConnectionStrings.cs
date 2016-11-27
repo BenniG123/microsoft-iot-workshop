0e1842d198edd1195a09d65817bccf704a665b7d
Ben Williams <bww1@iastate.edu>
2016-11-27 11:12:00 -0600

    Added in Azure connected app

---
+++ src/HelloCloud/ConnectionStrings.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloCloud
+{
+    public sealed partial class MainPage
+    {
+        static string iotHubUri = "Workshop-Hub.azure-devices.net";
+        static string deviceKey = "1w477TKFwNEOnc0J3rQ9vFmOvkerNWN8/bcyaIGiyj0=";
+        static string deviceId = "myThirdDevice";
+        static string connectionString = "HostName=Workshop-Hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=zZlNwbUsej7WfEnjWPhghUjp15i4xjd+6PTFHTkZZok=";
+    }
+}
---
+++ src/HelloCloud/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System.Diagnostics;
+using Microsoft.Azure.Devices.Client;
+
+namespace HelloCloud
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitDeviceAsync();
+        }
+
+        async void InitDeviceAsync()
+        {
+            Device device;
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            deviceKey = device.Authentication.SymmetricKey.PrimaryKey;
+            Debug.WriteLine(deviceKey);
+            progress.IsActive = false;
+
+            txtDeviceID.Text = deviceId;
+            txtIsConnected.Text = "Connected";
+        }
+
+        private async void btnSendMessage_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var deviceClient = DeviceClient.Create(iotHubUri,
+                    Microsoft.Azure.Devices.Client.AuthenticationMethodFactory.
+                        CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+                    Microsoft.Azure.Devices.Client.TransportType.Http1);
+
+            var str = boxMessage.Text;
+            var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(str));
+
+            progress.IsActive = true;
+            await deviceClient.SendEventAsync(message);
+            progress.IsActive = false;
+        }
+    }
+}
